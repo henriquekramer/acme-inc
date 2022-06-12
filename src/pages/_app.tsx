e2cdc6b2210d9bf560d82b3badc6845b58143418
Henrique Kramer <henriquemkramer@gmail.com>
2022-06-12 10:35:27 -0300

    Criação do hook useCart

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Header />
       <Component {...pageProps} />
+      <ToastContainer autoClose={3000}/>
       <Footer/>
     </>
   )
